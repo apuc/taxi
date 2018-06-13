07129b14873884927414ac755fdfa48a5faa7ea0
lupus1995 <canya.panfilov.95@mail.ru>
2018-06-13 10:29:05 +0300

    поправил логику форм регистрации и авторизации

--- frontend/modules/api/views/user/index.php
+++ frontend/modules/api/views/user/index.php
@@ -1,21 +1,27 @@
 <?php
 
-use yii\bootstrap\ActiveForm;
 use yii\helpers\Html;
-/* @var $this yii\web\View */
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View
+ * @var $model \frontend\models\SignupForm
+ */
 
 ?>
 <h1>Add user</h1>
 
 
-<?= Html::beginForm(['add'], 'post', ['data-pjax' => '', 'class' => 'form-inline']); ?>
-<p>Login</p>
-<?= Html::input('text', 'login', Yii::$app->request->post('login'), ['class' => 'form-control']) ?>
-<p>Email</p>
-<?= Html::input('text', 'email', Yii::$app->request->post('email'), ['class' => 'form-control']) ?>
-<p>Password</p>
-<?= Html::input('password', 'pass', Yii::$app->request->post('pass'), ['class' => 'form-control']) ?>
-<p>
-	<?= Html::submitButton('Send', ['class' => 'btn btn-lg btn-primary', 'name' => 'hash-button']) ?>
-</p>
+<?php $form = ActiveForm::begin( ); ?>
+
+<?= $form->field( $model, 'username' )->textInput( [ 'autofocus' => true ] ) ?>
+
+<?= $form->field( $model, 'email' ) ?>
+
+<?= $form->field( $model, 'password' )->passwordInput() ?>
+
+<div class="form-group">
+	<?= Html::submitButton( 'Signup', [ 'class' => 'btn btn-primary', 'name' => 'signup-button' ] ) ?>
+</div>
+
+<?php ActiveForm::end(); ?>
 
